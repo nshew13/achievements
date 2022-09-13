0d2e954ec5cc4d7e9b757bdbc8de8b87f7db4f93
Nicholas Shewmaker <nshewmaker@hotmail.com>
2022-09-13 13:24:57 -0400

    updated dependencies to silence Dependabot

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode:    'development',
+    mode: 'development',
     entry: {
         app: path.resolve(__dirname, 'src/index.ts'),
     },
@@ -13,28 +13,28 @@ module.exports = {
             // TODO: customize https://github.com/jantimon/html-webpack-plugin
             title: 'Achievement Coin',
             template: './src/index.html',
-//            inject: false,
+            //            inject: false,
         }),
     ],
-    output:  {
-        path:     path.resolve(__dirname, 'dist'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
     devtool: 'source-map',
     devServer: {
-        contentBase: './dist',
+        static: ['dist'],
     },
     resolve: {
         // for imports with no extension, resolve in this order
         extensions: ['.ts', '.js']
     },
-    module:  {
+    module: {
         rules: [
             {
-                test:    /\.ts$/,
+                test: /\.ts$/,
                 exclude: /node_modules/,
-                use:     {
-                    loader:  'ts-loader',
+                use: {
+                    loader: 'ts-loader',
                     options: {
                         transpileOnly: true
                     }
@@ -53,7 +53,7 @@ module.exports = {
                 test: /\.(png|jpg|gif)$/,
                 exclude: /node_modules/,
                 use: [
-                  'file-loader',
+                    'file-loader',
                 ],
             },
             // TODO: Get SVGs in the bundle
@@ -61,7 +61,7 @@ module.exports = {
                 test: /\.svg$/,
                 exclude: /node_modules/,
                 use: [
-                  'svg-inline-loader',
+                    'svg-inline-loader',
                 ],
             },
         ]
